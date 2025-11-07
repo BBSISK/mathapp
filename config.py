a0ccb7390d477a7a219dee8aead37037ce9834a9
Barry Sisk <barry.b.sisk@gmail.com>
2025-11-07 12:02:29 +0000

    Badging sorted

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+# Create this file in the same directory as app.py
+# Add your email credentials here
+
+# Email configuration
+MAIL_USERNAME = 'barry.b.sisk@gmail.com'
+MAIL_PASSWORD = 'yotnzywtrcbfbsbf'
+
+# Make sure to add config.py to .gitignore if using git!
